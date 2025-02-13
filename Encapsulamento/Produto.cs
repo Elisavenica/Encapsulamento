8e2ecd2704413435531947788abd0f68634d0b19
Elisa <elisac02@outlook.com>
2025-02-13 16:59:18 -0300

    melhorando implementação.

--- Encapsulamento/Produto.cs
+++ Encapsulamento/Produto.cs
@@ -5,26 +5,25 @@ namespace Course
     class Produto
     {
         private string _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
 
         public Produto()
         {
-            _quantidade = 5;
+            
         }
-
         public Produto(string nome, double preco, int quantidade)
         {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
         }
 
         public Produto(string nome, double preco)
         {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = 5;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
         }
         public string Nome
         {
@@ -36,31 +35,21 @@ namespace Course
             }
         }
 
-        public double Preco
-        {
-            get { return _preco; }
-        }
-        public int Quantidade
-        {
-
-            get { return _quantidade; }
-        }
-
         public double ValorTotalEmEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
         public void AdicionarProdutos(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }
         public override string ToString()
         {
-            return _nome
+            return Nome
                 + " , $ "
-                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + _quantidade
+                + Quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
