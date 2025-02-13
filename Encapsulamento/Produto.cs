73ba1a39e1d042d68fd8a69fe60f46910fa07c52
Elisa <elisac02@outlook.com>
2025-02-13 16:37:05 -0300

    Corrigindo as Propriedades.

--- Encapsulamento/Produto.cs
+++ Encapsulamento/Produto.cs
@@ -1,21 +1,71 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace Course
 {
-    class Program
+    class Produto
     {
-        static void Main(string[] args)
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
         {
-            Produto p = new Produto("TV", 500.00, 10);
-
-            p.Nome = ("TV 4K");
-
-            Console.WriteLine(p.Nome);
-            Console.WriteLine(p.GetPreco());
-            
-
+            _quantidade = 5;
         }
 
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 5;
+        }
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                    _nome = value;
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }
+        }
+        public int Quantidade
+        {
+
+            get { return _quantidade; }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+                + " , $ "
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
+
+
+
--- Encapsulamento/Program.cs
+++ Encapsulamento/Program.cs
@@ -1,71 +1,23 @@
-﻿using System.Globalization;
-
+﻿using System;
 using System.Globalization;
 
 namespace Course
 {
-    class Produto
+    class Program
     {
-        private string _nome;
-        private double _preco;
-        private int _quantidade;
-
-        public Produto()
+        static void Main(string[] args)
         {
-            _quantidade = 5;
-        }
+            Produto p = new Produto("TV", 500.00, 10);
 
-        public Produto(string nome, double preco, int quantidade)
-        {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            p.Nome = ("TV");
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+
+
         }
 
-        public Produto(string nome, double preco)
-        {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = 5;
-        }
-        public string Nome
-        {
-            get { return _nome; }
-            set
-            {
-                if (value != null && value.Length > 1)
-                    _nome = value;            }
-        }
-
-       // public
-        public double GetPreco()
-        {
-            return _preco;
-        }
-        public int GetQuantidade()
-        {
-            return _quantidade;
-        }
-        public double ValorTotalEmEstoque()
-        {
-            return _preco * _quantidade;
-        }
-        public void AdicionarProdutos(int quantidade)
-        {
-            _quantidade += quantidade;
-        }
-        public override string ToString()
-        {
-            return _nome
-                + " , $ "
-                + _preco.ToString("F2", CultureInfo.InvariantCulture)
-                + ", "
-                + _quantidade
-                + " unidades, Total: $ "
-                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
-        }
     }
 }
-
 
 
